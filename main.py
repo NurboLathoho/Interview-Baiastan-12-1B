2bb1329b937179fb4207f5550e4c3658ce83777a
parenok54 <parenok.game@gmail.com>
2024-02-08 20:17:33 +0600

    sobes

---
+++ main.py
@@ -0,0 +1,22 @@
+# Написать функцию, которая выводит на экран циклом пять строк из нулей, причем каждая строка должна быть пронумерована. 1
+# Напишите функцию, которая принимает список, из списка выдает случайное значение из списка и записывает результат в txt файл. Список language = ["Python", "Java", "Kotlin", "JavaScript", "C#","RUBY"] 1
+# names = [“azat”, “zina”, “kuma”, “anna”, “sas”] Вывести с помощью lambda функции имена палиндромы 1
+ 
+# for number in range(1,6):
+#     print(f'0 {number}')
+
+
+# import random
+
+# def random_slovo(input_list, filename):
+#     random_value = random.choice(input_list)
+#     with open(filename, 'w') as file:
+#         file.write(random_value)
+
+# language = ["Python", "Java", "Kotlin", "JavaScript", "C#", "RUBY"]
+# random_slovo(language, "output.txt")
+
+
+# name = ["azat", "zina", "kuma", "anna", "sas"]
+# palidrom = filter(lambda x: x == x[::-1], name)
+# print(list(palidrom))
